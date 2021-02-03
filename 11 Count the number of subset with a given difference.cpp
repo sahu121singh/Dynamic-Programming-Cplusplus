cf800ca13b8b9d3d1a115358a836dbe9b6f41c33
Ashrender_Kumar_Gupta <44531366+sahu121singh@users.noreply.github.com>
2021-02-03 12:32:48 +0530

    Add files via upload

---
+++ 11 Count the number of subset with a given difference.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fun(int arr[], int n, int sm){
+    
+    int dp[n+1][sm+1];
+    memset(dp, 0, sizeof(dp));
+    dp[0][0] = 1;
+    
+    for(int i = 1; i<n+1; i++){
+        for(int j = 0; j < sm+1; j++){
+            if(j == 0){
+                dp[i][j] = 1;
+            }
+            else if(arr[i-1] > j){
+                dp[i][j] = dp[i-1][j];
+            }
+            else{
+                dp[i][j] = dp[i-1][j-arr[i-1]] + dp[i-1][j];
+            }
+        }
+    }
+    for(int i = 0; i<n+1; i++){
+        for(int j = 0; j < sm+1; j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return dp[n][sm];
+}
+
+int fun1(int arr[], int n, int sm){
+    if(n==0 and sm != 0){
+        return 0;
+    }
+    if(n == 0 or sm == 0){
+        return 1;
+    }
+    if(arr[n-1] > sm){
+        return fun1(arr, n-1, sm);
+    }
+    else{
+        return fun1(arr, n-1, sm-arr[n-1]) + fun1(arr, n-1, sm);
+    }
+}
+
+
+int main(){
+    int arr[] = {1, 1, 2, 3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    int sm = 0;
+    for(int i = 0; i < n; i++){
+        sm += arr[i];
+    }
+    
+    int find = (1+sm)/2;
+    cout<<fun(arr, n, find);
+}
