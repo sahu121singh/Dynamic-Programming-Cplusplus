6a74068a13f62c4a5189ef75ab36150a190d2b50
Ashrender_Kumar_Gupta <44531366+sahu121singh@users.noreply.github.com>
2021-02-17 00:48:35 +0530

    Add files via upload

---
+++ 29 Print shortest common Supersequence.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string fun(string s1, string s2, int n, int m){
+    int dp[n+1][m+1];
+    memset(dp, 0, sizeof(dp));
+    
+    for(int i = 0; i < n+1; i++){
+        for(int j = 0; j < m+1; j++){
+            if(i==0 || j==0){
+                continue;
+            }
+            if(s1[i-1] == s2[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+    }
+    for(int i = 0; i < n+1; i++){
+        for(int j = 0; j < m+1; j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    
+    string res = "";
+    int i = n;
+    int j = m;
+    while(i > 0 && j > 0){
+        if(s1[i-1] == s2[j-1]){
+            res = s1[i-1] + res;
+            i-=1;
+            j-=1;
+        }
+        else{
+            if(dp[i][j-1] > dp[i-1][j]){
+                res = s2[j-1] + res;
+                j-=1;
+            }
+            else{
+                res = s1[i-1] + res;
+                i-=1;
+            }
+        }
+    }
+    return res;
+}
+
+int main(){
+    string S1 = "abckjnsdksdkdgh";
+    string S2 = "abesjdnksddl";
+    cout<<fun(S1, S2, S1.length(), S2.length());
+}
