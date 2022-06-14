2a6f6c5ab13972c9ed96e39062167af7936a372a
Ashrender_Kumar_Gupta <44531366+sahu121singh@users.noreply.github.com>
2022-06-14 23:13:46 +0530

    Update 13 Unbounded Knapsack.cpp

--- 13 Unbounded Knapsack.cpp
+++ 13 Unbounded Knapsack.cpp
@@ -5,11 +5,8 @@ int fun(int *val, int *wt, int n, int w){
     int dp[n+1][w+1];
     memset(dp, 0, sizeof(dp));
     
-    for(int i = 0; i < n+1; i++){
-        for(int j = 0; j < w+1; j++){
-            if(i==0 or j == 0){
-                continue;
-            }
+    for(int i = 1; i < n+1; i++){
+        for(int j = 1; j < w+1; j++){
             if(wt[i-1] > j){
                 dp[i][j] = dp[i-1][j];
             }
